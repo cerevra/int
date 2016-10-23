74befedb50eab27fc0e81e1ac1d6905fd31ade2d
Igor Klevanets <cerevra@yandex-team.ru>
2016-10-23 18:23:56 +0300

    Shift left and right

---
+++ v2/int.h
@@ -0,0 +1,186 @@
+#pragma once
+
+#include <stdexcept>
+#include <stdint.h>
+#include <type_traits>
+#include <ostream>
+#include <sstream>
+#include <iomanip>
+#include <limits>
+
+
+#include <iostream>
+
+constexpr inline bool is_power_of_two(uint16_t bits) {
+    return bits == 0x0002 ||
+           bits == 0x0004 ||
+           bits == 0x0008 ||
+           bits == 0x0010 ||
+           bits == 0x0020 ||
+           bits == 0x0040 ||
+           bits == 0x0080 ||
+           bits == 0x0100 ||
+           bits == 0x0200 ||
+           bits == 0x0400 ||
+           bits == 0x0800 ||
+           bits == 0x1000 ||
+           bits == 0x2000 ||
+           bits == 0x4000 ||
+           bits == 0x8000;
+}
+
+template<int bits, bool sgn>
+class wide_int {
+public:
+    static_assert(bits >= 128, "");
+    static_assert(is_power_of_two(bits), "");
+    using base_type = uint64_t;
+    constexpr static int arr_size() noexcept {
+        return bits / 64;
+    }
+    constexpr static int base_bits() noexcept {
+        return sizeof(base_type)*8;
+    }
+
+    wide_int() = default;
+    template<typename T>
+    wide_int(T other) noexcept {
+        int i;
+        for (i = 0; i < arr_size() - 1; ++i) {
+            m_arr[i] = other < 0 ? std::numeric_limits<uint64_t>::max() : 0;
+        }
+        m_arr[i] = static_cast<uint64_t>(other);
+    }
+    template<int bits2, bool sgn2>
+    wide_int(wide_int<bits2,sgn2> other) noexcept {
+        int bytes_to_copy = std::min(arr_size(), other.arr_size());
+        for (int i = 0; i < bytes_to_copy; ++i) {
+            m_arr[arr_size() - 1 - i] = other.m_arr[other.arr_size() - 1 - i];
+        }
+        bool is_negative = sgn2 && static_cast<int64_t>(other.m_arr[0]) < 0;
+        for (int i = 0; i < arr_size() - bytes_to_copy; ++i) {
+            m_arr[i] = sgn && is_negative ? std::numeric_limits<base_type>::max() : 0;
+        }
+    }
+
+    wide_int(const wide_int<bits,sgn>&) = default;
+
+    constexpr static wide_int<bits,false> shift_left(const wide_int<bits,false>& other, int n) noexcept {
+        if (n >= bits) return 0;
+        if (n <= 0) return other;
+
+        wide_int<bits,sgn> num = other;
+        int cur_shift = n % base_bits();
+        if (cur_shift) {
+            num.m_arr[0] <<= cur_shift;
+            for (int i = 1; i < arr_size(); ++i) {
+                num.m_arr[i-1]  |= num.m_arr[i] >> (base_bits() - cur_shift);
+                num.m_arr[i  ] <<= cur_shift;
+            }
+            n -= cur_shift;
+        }
+        if (n) {
+            int i = 0;
+            for (; i < arr_size() - n/base_bits(); ++i) {
+                num.m_arr[i] = num.m_arr[i + n/base_bits()];
+            }
+            for (; i < arr_size(); ++i) {
+                num.m_arr[i] = 0;
+            }
+        }
+        return num;
+    }
+
+    constexpr static wide_int<bits,true> shift_left(const wide_int<bits,true>& , int ) {
+        throw std::runtime_error("shift left for signed numbers is underfined!");
+    }
+
+    constexpr static wide_int<bits,false> shift_right(const wide_int<bits,false>& other, int n) noexcept {
+        if (n >= bits) return 0;
+        if (n <= 0) return other;
+
+        wide_int<bits,sgn> num = other;
+        int cur_shift = n % base_bits();
+        if (cur_shift) {
+            num.m_arr[arr_size() - 1] >>= cur_shift;
+            for (int i = arr_size() - 2; i >= 0; --i) {
+                num.m_arr[i+1]  |= num.m_arr[i] << (base_bits() - cur_shift);
+                num.m_arr[i  ] >>= cur_shift;
+            }
+            n -= cur_shift;
+        }
+        if (n) {
+            int i = arr_size() - 1;
+            for (; i >= static_cast<int>(n/base_bits()); --i) {
+                num.m_arr[i] = num.m_arr[i - n/base_bits()];
+            }
+            for (; i >= 0; --i) {
+                num.m_arr[i] = 0;
+            }
+        }
+        return num;
+    }
+
+private:
+    constexpr static void shift_right_64(uint64_t& num, int n, bool is_negative) noexcept {
+        if (is_negative) {
+            num = int64_t(num) >> n;
+        } else {
+            num >>= n;
+        }
+    }
+public:
+
+    constexpr static wide_int<bits,true> shift_right(const wide_int<bits,true>& other, int n) noexcept {
+        if (n >= bits) return 0;
+        if (n <= 0) return other;
+
+        bool is_negative = static_cast<int64_t>(other.m_arr[0]) < 0;
+        if (!is_negative) {
+            return shift_right(wide_int<bits,false>(other), n);
+        }
+
+        wide_int<bits,sgn> num = other;
+        int cur_shift = n % base_bits();
+        if (cur_shift) {
+            int idx = 0;
+            while(num.m_arr[idx] == std::numeric_limits<base_type>::max()) {
+                ++idx;
+            }
+            bool preamb = is_negative;
+            base_type prev = is_negative ? std::numeric_limits<base_type>::max() : 0;
+            for (; idx < arr_size(); ++idx) {
+                base_type curr = num.m_arr[idx];
+                shift_right_64(num.m_arr[idx], cur_shift, preamb);
+                num.m_arr[idx] |= prev << (sizeof(base_type)*8 - cur_shift);
+                prev = curr;
+                if (num.m_arr[idx] != std::numeric_limits<base_type>::max()) {
+                    preamb = false;
+                }
+            }
+            n -= cur_shift;
+        }
+        if (n) {
+            int i = arr_size() - 1;
+            for (; i >= static_cast<int>(n/base_bits()); --i) {
+                num.m_arr[i] = num.m_arr[i - n/base_bits()];
+            }
+            for (; i >= 0; --i) {
+                num.m_arr[i] = is_negative ? std::numeric_limits<uint64_t>::max() : 0;
+            }
+        }
+        return num;
+    }
+
+//private:
+    uint64_t m_arr[arr_size()];
+};
+
+using int128_t = wide_int<128,true>;
+using uint128_t = wide_int<128,false>;
+
+using int256_t = wide_int<256,true>;
+using uint256_t = wide_int<256,false>;
+
+using int512_t = wide_int<512,true>;
+using uint512_t = wide_int<512,false>;
---
+++ v2/main.cpp
@@ -0,0 +1,392 @@
+
+#include "int.h"
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+#define assert(a, info) {if (!(a)) throw std::runtime_error(#a ", line:" + std::to_string(__LINE__) + ", info:" + info );}
+
+void testCtors() {
+    int512_t a1 = 0;
+    for (auto c : a1.m_arr) {
+        assert(c == 0, "");
+    }
+    uint512_t b1 = 0;
+    for (auto c : b1.m_arr) {
+        assert(c == 0, "");
+    }
+
+    int512_t a2 = 13;
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(a2.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a2.m_arr[7] == 13, "");
+    uint512_t b2 = 13;
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(b2.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b2.m_arr[7] == 13, "");
+
+    int512_t a3 = -13;
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(a3.m_arr[idx] == 0xffffffffffffffff, std::to_string(idx));
+    }
+    assert(a3.m_arr[7] == 0xfffffffffffffff3, "");
+    uint512_t b3 = -13;
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(b3.m_arr[idx] == 0xffffffffffffffff, std::to_string(idx));
+    }
+    assert(b3.m_arr[7] == 0xfffffffffffffff3, "");
+
+    int512_t a4 = int128_t(13);
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(a4.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a4.m_arr[7] == 13, "");
+    uint512_t b4 = uint128_t(13);
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(b4.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b4.m_arr[7] == 13, "");
+
+    int512_t a5 = uint128_t(13);
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(a5.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a5.m_arr[7] == 13, "");
+    uint512_t b5 = int128_t(13);
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(b5.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b5.m_arr[7] == 13, "");
+
+    int512_t a6 = uint128_t(-13);
+    for (int idx = 0; idx < 6; ++idx) {
+        assert(a6.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a6.m_arr[6] == 0xffffffffffffffff, "");
+    assert(a6.m_arr[7] == 0xfffffffffffffff3, "");
+    uint512_t b6 = int128_t(-13);
+    for (int idx = 0; idx < 6; ++idx) {
+        assert(b6.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a6.m_arr[6] == 0xffffffffffffffff, "");
+    assert(b6.m_arr[7] == 0xfffffffffffffff3, "");
+
+    int512_t a7 = int128_t(-13);
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(a7.m_arr[idx] == 0xffffffffffffffff, std::to_string(idx));
+    }
+    assert(a7.m_arr[7] == 0xfffffffffffffff3, "");
+    uint512_t b7 = uint128_t(-13);
+    for (int idx = 0; idx < 6; ++idx) {
+        assert(b7.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b7.m_arr[6] == 0xffffffffffffffff, "");
+    assert(b7.m_arr[7] == 0xfffffffffffffff3, "");
+}
+
+void testShifts() {
+    //left
+    uint512_t a1 = 0;
+    for (auto c : uint512_t::shift_left(a1, 0).m_arr) {
+        assert(c == 0, "");
+    }
+    uint512_t a2 = 0;
+    for (auto c : uint512_t::shift_left(a2, 15).m_arr) {
+        assert(c == 0, "");
+    }
+
+    uint512_t a3 = 0xf1f2f3f4f5f6f7f8;
+    uint512_t a31 = uint512_t::shift_left(a3, 8);
+    for (int idx = 0; idx < 6; ++idx) {
+        assert(a31.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a31.m_arr[6] == 0x00000000000000f1, "");
+    assert(a31.m_arr[7] == 0xf2f3f4f5f6f7f800, "");
+
+    uint512_t a32 = uint512_t::shift_left(a3, 16);
+    for (int idx = 0; idx < 6; ++idx) {
+        assert(a32.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a32.m_arr[6] == 0x000000000000f1f2, "");
+    assert(a32.m_arr[7] == 0xf3f4f5f6f7f80000, "");
+
+    uint512_t a33 = uint512_t::shift_left(a3, 4);
+    for (int idx = 0; idx < 6; ++idx) {
+        assert(a33.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a33.m_arr[6] == 0x000000000000000f, "");
+    assert(a33.m_arr[7] == 0x1f2f3f4f5f6f7f80, "");
+
+    uint512_t a34 = uint512_t::shift_left(a3, 64);
+    for (int idx = 0; idx < 6; ++idx) {
+        assert(a34.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a34.m_arr[6] == 0xf1f2f3f4f5f6f7f8, "");
+    assert(a34.m_arr[7] == 0, "");
+
+    uint512_t a35 = uint512_t::shift_left(a3, 256);
+    for (int idx = 0; idx < 3; ++idx) {
+        assert(a35.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a35.m_arr[3] == 0xf1f2f3f4f5f6f7f8, "");
+    for (int idx = 4; idx < 8; ++idx) {
+        assert(a35.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    uint512_t a36 = uint512_t::shift_left(a3, 260);
+    assert(a36.m_arr[0] == 0, "");
+    assert(a36.m_arr[1] == 0, "");
+    assert(a36.m_arr[2] == 0x000000000000000f, "");
+    assert(a36.m_arr[3] == 0x1f2f3f4f5f6f7f80, "");
+    for (int idx = 4; idx < 8; ++idx) {
+        assert(a36.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    //right uint
+    uint512_t a4 = 0;
+    for (auto c : uint512_t::shift_right(a4, 0).m_arr) {
+        assert(c == 0, "");
+    }
+    uint512_t a5 = 0;
+    for (auto c : uint512_t::shift_right(a5, 15).m_arr) {
+        assert(c == 0, "");
+    }
+
+    uint512_t a6 = 0xf1f2f3f4f5f6f7f8;
+    a6.m_arr[0] = 0xf1f2f3f4f5f6f7f8;
+    uint512_t a61 = uint512_t::shift_right(a6, 8);
+    assert(a61.m_arr[0] == 0x00f1f2f3f4f5f6f7, "");
+    assert(a61.m_arr[1] == 0xf800000000000000, "");
+    for (int idx = 2; idx < 7; ++idx) {
+        assert(a61.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a61.m_arr[7] == 0x00f1f2f3f4f5f6f7, "");
+
+    uint512_t a62 = uint512_t::shift_right(a6, 16);
+    assert(a62.m_arr[0] == 0x0000f1f2f3f4f5f6, "");
+    assert(a62.m_arr[1] == 0xf7f8000000000000, "");
+    for (int idx = 2; idx < 6; ++idx) {
+        assert(a62.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a62.m_arr[7] == 0x0000f1f2f3f4f5f6, "");
+
+    uint512_t a63 = uint512_t::shift_right(a6, 4);
+    assert(a63.m_arr[0] == 0x0f1f2f3f4f5f6f7f, "");
+    assert(a63.m_arr[1] == 0x8000000000000000, "");
+    for (int idx = 2; idx < 6; ++idx) {
+        assert(a63.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a63.m_arr[7] == 0x0f1f2f3f4f5f6f7f, "");
+
+    uint512_t a64 = uint512_t::shift_right(a6, 64);
+    assert(a64.m_arr[0] == 0, "");
+    assert(a64.m_arr[1] == 0xf1f2f3f4f5f6f7f8, "");
+    for (int idx = 2; idx < 7; ++idx) {
+        assert(a64.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    uint512_t a65 = uint512_t::shift_right(a6, 256);
+    for (int idx = 0; idx < 4; ++idx) {
+        assert(a65.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a65.m_arr[4] == 0xf1f2f3f4f5f6f7f8, "");
+    for (int idx = 5; idx < 8; ++idx) {
+        assert(a65.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    uint512_t a66 = uint512_t::shift_right(a6, 260);
+    for (int idx = 0; idx < 4; ++idx) {
+        assert(a66.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(a66.m_arr[4] == 0x0f1f2f3f4f5f6f7f, "");
+    assert(a66.m_arr[5] == 0x8000000000000000, "");
+    for (int idx = 6; idx < 8; ++idx) {
+        assert(a66.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    //right int > 0
+    int512_t b4 = 0;
+    for (auto c : int512_t::shift_right(b4, 0).m_arr) {
+        assert(c == 0, "");
+    }
+    int512_t b5 = 0;
+    for (auto c : int512_t::shift_right(b5, 15).m_arr) {
+        assert(c == 0, "");
+    }
+
+    int512_t b6 = 0xf1f2f3f4f5f6f7f8;
+    b6.m_arr[0] = 0x71f2f3f4f5f6f7f8;
+    int512_t b61 = int512_t::shift_right(b6, 8);
+    assert(b61.m_arr[0] == 0x0071f2f3f4f5f6f7, "");
+    assert(b61.m_arr[1] == 0xf800000000000000, "");
+    for (int idx = 2; idx < 7; ++idx) {
+        assert(b61.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b61.m_arr[7] == 0x00f1f2f3f4f5f6f7, "");
+
+    int512_t b62 = int512_t::shift_right(b6, 16);
+    assert(b62.m_arr[0] == 0x000071f2f3f4f5f6, "");
+    assert(b62.m_arr[1] == 0xf7f8000000000000, "");
+    for (int idx = 2; idx < 6; ++idx) {
+        assert(b62.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b62.m_arr[7] == 0x0000f1f2f3f4f5f6, "");
+
+    int512_t b63 = int512_t::shift_right(b6, 4);
+    assert(b63.m_arr[0] == 0x071f2f3f4f5f6f7f, "");
+    assert(b63.m_arr[1] == 0x8000000000000000, "");
+    for (int idx = 2; idx < 6; ++idx) {
+        assert(b63.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b63.m_arr[7] == 0x0f1f2f3f4f5f6f7f, "");
+
+    int512_t b64 = int512_t::shift_right(b6, 64);
+    assert(b64.m_arr[0] == 0, "");
+    assert(b64.m_arr[1] == 0x71f2f3f4f5f6f7f8, "");
+    for (int idx = 2; idx < 7; ++idx) {
+        assert(b64.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    int512_t b65 = int512_t::shift_right(b6, 256);
+    for (int idx = 0; idx < 4; ++idx) {
+        assert(b65.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b65.m_arr[4] == 0x71f2f3f4f5f6f7f8, "");
+    for (int idx = 5; idx < 8; ++idx) {
+        assert(b65.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    int512_t b66 = int512_t::shift_right(b6, 260);
+    for (int idx = 0; idx < 4; ++idx) {
+        assert(b66.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b66.m_arr[4] == 0x071f2f3f4f5f6f7f, "");
+    assert(b66.m_arr[5] == 0x8000000000000000, "");
+    for (int idx = 6; idx < 8; ++idx) {
+        assert(b66.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    //right int < 0
+    int512_t b7 = 0xf1f2f3f4f5f6f7f8;
+    b7.m_arr[0] = 0xf1f2f3f4f5f6f7f8;
+    int512_t b71 = int512_t::shift_right(b7, 8);
+    assert(b71.m_arr[0] == 0xfff1f2f3f4f5f6f7, "");
+    assert(b71.m_arr[1] == 0xf800000000000000, "");
+    for (int idx = 2; idx < 7; ++idx) {
+        assert(b71.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b71.m_arr[7] == 0x00f1f2f3f4f5f6f7, "");
+
+    int512_t b72 = int512_t::shift_right(b7, 16);
+    assert(b72.m_arr[0] == 0xfffff1f2f3f4f5f6, "");
+    assert(b72.m_arr[1] == 0xf7f8000000000000, "");
+    for (int idx = 2; idx < 6; ++idx) {
+        assert(b72.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b72.m_arr[7] == 0x0000f1f2f3f4f5f6, "");
+
+    int512_t b73 = int512_t::shift_right(b7, 4);
+    assert(b73.m_arr[0] == 0xff1f2f3f4f5f6f7f, "");
+    assert(b73.m_arr[1] == 0x8000000000000000, "");
+    for (int idx = 2; idx < 6; ++idx) {
+        assert(b73.m_arr[idx] == 0, std::to_string(idx));
+    }
+    assert(b73.m_arr[7] == 0x0f1f2f3f4f5f6f7f, "");
+
+    int512_t b74 = int512_t::shift_right(b7, 64);
+    assert(b74.m_arr[0] == 0xffffffffffffffff, "");
+    assert(b74.m_arr[1] == 0xf1f2f3f4f5f6f7f8, "");
+    for (int idx = 2; idx < 7; ++idx) {
+        assert(b74.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    int512_t b75 = int512_t::shift_right(b7, 256);
+    for (int idx = 0; idx < 4; ++idx) {
+        assert(b75.m_arr[idx] == 0xffffffffffffffff, std::to_string(idx));
+    }
+    assert(b75.m_arr[4] == 0xf1f2f3f4f5f6f7f8, "");
+    for (int idx = 5; idx < 8; ++idx) {
+        assert(b75.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    int512_t b76 = int512_t::shift_right(b7, 260);
+    for (int idx = 0; idx < 4; ++idx) {
+        assert(b76.m_arr[idx] == 0xffffffffffffffff, std::to_string(idx));
+    }
+    assert(b76.m_arr[4] == 0xff1f2f3f4f5f6f7f, "");
+    assert(b76.m_arr[5] == 0x8000000000000000, "");
+    for (int idx = 6; idx < 8; ++idx) {
+        assert(b76.m_arr[idx] == 0, std::to_string(idx));
+    }
+
+    int512_t b8 = -13;
+    int512_t b81 = int512_t::shift_right(b8, 1);
+    for (int idx = 0; idx < 7; ++idx) {
+        assert(b81.m_arr[idx] == 0xffffffffffffffff, std::to_string(idx));
+    }
+    assert(b81.m_arr[7] == 0xfffffffffffffff9, "");
+}
+
+void tests() {
+    static_assert(std::is_pod<int512_t>::value, "");
+    static_assert(std::is_pod<uint512_t>::value, "");
+    static_assert(std::is_standard_layout<int512_t>::value, "");
+    static_assert(std::is_standard_layout<uint512_t>::value, "");
+
+    testCtors();
+    testShifts();
+}
+
+int main() {
+    tests();
+
+    wide_int<512, true> a(13);
+    a.m_arr[0] = std::numeric_limits<uint64_t>::max();
+//    wide_int<128> b = 18;
+    std::cout << "--- " << (wide_int<512, true>::shift_right(a, 0)).m_arr[7] << std::endl;
+
+    for (uint64_t u : wide_int<512, true>::shift_right(a, 0).m_arr) {
+        std::cout <<std::hex << std::setw(16) << std::setfill('0') << u << std::endl;
+    }
+
+    //    std::cout << b << std::endl;
+
+//    wide_int<256> c = 200;
+//    std::cout << c << std::endl;
+
+//    wide_int<512> d = 400;
+//    std::cout << d << std::endl;
+//    d = 15;
+//    std::cout << d << std::endl;
+//    d = c + 22;
+//    std::cout << d << std::endl;
+//    d = 20 + c;
+//    std::cout << d << std::endl;
+
+//    // TODO
+//    std::cout << std::numeric_limits<wide_int<512>>::max() << std::endl;
+
+//    std::cout << 111123123123123123123_uint128 << std::endl;
+
+//    std::cout << std::hex << std::setw(16) << std::setfill('0');
+//    std::cout << -int64_t(13) << std::endl;
+//    std::cout << std::hex << std::setw(16) << std::setfill('0');
+//    std::cout << (-int64_t(0xc0c0) >> 1) << std::endl;
+
+//    std::cout << std::hex << std::setw(16) << std::setfill('0');
+//    std::cout << int64_t(-1) << std::endl;
+//    std::cout << std::hex << std::setw(16) << std::setfill('0');
+//    std::cout << (int64_t(-1) << 1) << std::endl;
+//    std::cout << std::hex << std::setw(16) << std::setfill('0');
+//    std::cout << (int64_t(-1) << 16) << std::endl;
+//    std::cout << std::hex << std::setw(16) << std::setfill('0');
+//    std::cout << (int64_t(-1) >> 8) << std::endl;
+//    std::cout << std::hex << std::setw(16) << std::setfill('0');
+//    std::cout << (int64_t(-1) >> 16) << std::endl;
+
+    return 0;
+}
